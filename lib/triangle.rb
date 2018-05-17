ce38b290637431db1a0606b4f54b2bacdc37653b
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-05-17 17:19:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind
-    if @s1 == @s2
+    if @s1 + @s2 < @s3 || @s1 + @s3 < @s2 || @s2 + @s3 < @s1 || @s1 <= 0 || @s2 <= 0 || @s3 <= 0
+      raise TriangleError
+    elsif @s1 == @s2
       if @s1 == @s3
         return :equilateral
       else
@@ -20,5 +22,9 @@ class Triangle
       return :scalene
     end
   end
-      
+
+end
+
+class TriangleError < StandardError
+  
 end
