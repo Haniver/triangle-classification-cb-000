c36b4f13b5c810324f6f9c873d8d87366bd20a83
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-05-17 17:13:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 == @s2
+      if @s1 == @s3
+        return :equilateral
+      else
+        return :isosceles
+      end
+    elsif @s1 == @s3 || @s2 == @s3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+      
 end
